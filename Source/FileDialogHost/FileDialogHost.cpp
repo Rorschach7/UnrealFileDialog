62128c4f5453add718ef32da8ee99eeb3e44ceaa
Kevin Lichtenberg <kevlicht@gmail.com>
2025-10-31 09:16:48 +0100

    Init Unreal host project

---
+++ Source/FileDialogHost/FileDialogHost.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FileDialogHost.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FileDialogHost, "FileDialogHost" );
